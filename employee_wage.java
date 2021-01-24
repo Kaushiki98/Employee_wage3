7c5d3054196405d98dc49a9781ed921e48217352
kaushiki <kaushiki.ak88@gmail.com>
2021-01-24 18:17:32 +0530

    Welcome message

---
+++ employee_wage.java
@@ -0,0 +1,10 @@
+public class employee_wage{
+	private void doCalculation()
+	{
+		System.out.println(" WElcome to employee wage calculation ");
+	}
+	public static void main(String[] args){
+		employee_wage Emp = new employee_wage();
+		Emp.doCalculation();
+	}
+}

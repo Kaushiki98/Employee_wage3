48c3c74fbfa96e69345e67348b19a73a0a7142ee
kaushiki <kaushiki.ak88@gmail.com>
2021-01-24 20:01:34 +0530

    Employee wage using switch case

--- employee_wage.java
+++ employee_wage.java
@@ -2,28 +2,32 @@ public class employee_wage{
 
 	static final int is_Full_Time = 1;
 	static final int is_Part_Time = 2;
-	static final int emp_wage_per_hr = 20;
-	static final int no_of_hrs = 8;
-	static final int no_of_part_time_hrs = 4;
+	static final int emp_wage_per_hour = 20;
+	static final int no_of_hours = 8;
+	static final int no_of_part_time_hours = 4;
 
 	private void doCalculation()
 	{
 		System.out.println(" Welcome to employee wage calculation ");
+
 		int emp_attendance = (int)(Math.floor(Math.random()*10) % 3);
 
-		if( emp_attendance == is_Full_Time ){
-			System.out.println(" Employee is full time ");
-			int Payment = emp_wage_per_hr * no_of_hrs;
-			System.out.println(" Payment is : " +Payment );
-		}
-		else if( emp_attendance == is_Part_Time ){
-         System.out.println(" Employee is part time ");
-         int Payment = emp_wage_per_hr * no_of_part_time_hrs;
-         System.out.println(" Payment is : " +Payment );
-		}
-		else {
-			System.out.println(" Employee is abscent ");
-			System.out.println(" Payment is 0 ");
+		switch (emp_attendance) {
+
+		case 0:
+			System.out.println("Employee is present ");
+			int Payment = emp_wage_per_hour * no_of_hours;
+			System.out.println("Payment is: "+ Payment);
+			break;
+		case 1:
+			System.out.println("Employee is part time");
+			int Payments = emp_wage_per_hour * no_of_part_time_hours;
+			System.out.println("Part time Payment is: "+ Payments);
+			break;
+		case 2:
+			System.out.println("Employee is Abscent");
+			System.out.println("Payment is 0");
+			break;
 
 		}
 	}

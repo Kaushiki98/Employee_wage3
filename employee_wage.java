d93dce32b27446844121fdf068a5707930eeb60a
kaushiki <kaushiki.ak88@gmail.com>
2021-01-24 19:28:59 +0530

    Wages of the employees

--- employee_wage.java
+++ employee_wage.java
@@ -1,16 +1,24 @@
 public class employee_wage{
 
 	static final int is_Full_Time = 1;
+	static final int emp_wage_per_hr = 20;
+	static final int no_of_hrs = 8;
 
 	private void doCalculation()
 	{
 		System.out.println(" Welcome to employee wage calculation ");
 		int emp_attendance = (int)(Math.floor(Math.random()*10) % 2);
-		if( emp_attendance == 1 ){
+
+		if( emp_attendance == is_Full_Time ){
 			System.out.println(" Employee is present ");
+			int Payment = emp_wage_per_hr * no_of_hrs;
+			System.out.println(" Payment is : " +Payment );
+
 		}
 		else {
 			System.out.println(" Employee is abscent ");
+			System.out.println(" Payment is 0 ");
+
 		}
 	}
 

7208cb4991f7f523b2d94d04325aef3c8b50cf9e
kaushiki <kaushiki.ak88@gmail.com>
2021-01-24 18:57:58 +0530

    Attendance of the employees

--- employee_wage.java
+++ employee_wage.java
@@ -1,8 +1,19 @@
 public class employee_wage{
+
+	static final int is_Full_Time = 1;
+
 	private void doCalculation()
 	{
-		System.out.println(" WElcome to employee wage calculation ");
+		System.out.println(" Welcome to employee wage calculation ");
+		int emp_attendance = (int)(Math.floor(Math.random()*10) % 2);
+		if( emp_attendance == 1 ){
+			System.out.println(" Employee is present ");
+		}
+		else {
+			System.out.println(" Employee is abscent ");
+		}
 	}
+
 	public static void main(String[] args){
 		employee_wage Emp = new employee_wage();
 		Emp.doCalculation();

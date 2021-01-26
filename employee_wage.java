c26ed748cf94339094505c83eb63f5cc9c936809
kaushiki <kaushiki.ak88@gmail.com>
2021-01-26 16:40:28 +0530

    Employee wage for company

--- employee_wage.java
+++ employee_wage.java
@@ -3,29 +3,50 @@ public class employee_wage{
 	public static final int is_Full_Time = 1;
 	public static final int is_Part_Time = 2;
 
-	public void calEmpWage(int EmpWage, int NumOfDays , int maxHrs)
+	public final String company;
+	public final int Emp_wage_per_hr;
+	public final int num_of_days;
+	public final int max_hrs;
+
+	public employee_wage(String company, int Emp_wage_per_hr, int num_of_days, int max_hrs)
+	{
+		this.company = company;
+		this.Emp_wage_per_hr = Emp_wage_per_hr;
+		this.num_of_days = num_of_days;
+		this.max_hrs = max_hrs;
+	}
+
+	public static void calEmpWage(int Emp_wage_per_hr, int num_of_days, int max_hrs)
 	{
 
-		int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+		int Emp_total_wage = 0 ;
 
-		if(empCheck == is_Full_Time){
+		int empCheck = (int) (Math.floor(Math.random() * 10) % 3 );
+
+		if(empCheck == is_Full_Time)
+		{
 			System.out.println("Employee is Full time ");
-			int Payment = EmpWage * maxHrs * NumOfDays;
-			System.out.println("Full time Payment for one month: "+ Payment);
+			Emp_total_wage = Emp_wage_per_hr * max_hrs * num_of_days;
+			System.out.println("Full time Payment for one month: "+ Emp_total_wage);
 		}
 
-		else if(empCheck == is_Part_Time){
+		else if(empCheck == is_Part_Time)
+		{
 			System.out.println("Employee is part timr: ");
-         int Payment = EmpWage * maxHrs * NumOfDays;
-         System.out.println("Full time Payment for one month: "+ Payment);
+         Emp_total_wage = Emp_wage_per_hr * max_hrs / 2 * num_of_days;
+         System.out.println("Full time Payment for one month: "+ Emp_total_wage);
 		}
-		else{
+		else
+		{
 			System.out.println("Employee is Abscent");
 		}
 	}
 
-	public static void main(String[] args){
-		employee_wage company = new employee_wage ();
-		company.calEmpWage(80,2,8);
+	public static void main(String[] args)
+	{
+		System.out.println(" Welcome to Employee wage Calculation ");
+		employee_wage dmart = new employee_wage ("Dmart", 20, 20, 100);
+		System.out.println(" The employee wage for company " + dmart.company + " wage per hour is " + dmart.Emp_wage_per_hr +" the number of days are " +dmart.num_of_days +" and maximum hours are "+ dmart.max_hrs + " hours ");
+		dmart.calEmpWage (20, 20, 100);
 	}
 }
